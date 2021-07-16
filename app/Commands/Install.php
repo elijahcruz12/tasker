f2f9d9f4027f12124e29edc86f4dea66bb429e51
Elijah Cruz <ecwebservices@gmail.com>
2021-07-15 21:56:08 -0500

    Updated Tasker to include an install command.

---
+++ app/Commands/Install.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use LaravelZero\Framework\Commands\Command;
+
+class Install extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'install';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Installs taskers required folders just for your user.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+        $this->info('Installing Tasker');
+
+        $bar = $this->output->createProgressBar(5);
+
+        $taskerHome = $_SERVER['HOME'] . '/.tasker';
+
+        $this->task('Creating folder ' . $taskerHome, function () {
+            sleep(1);
+
+            $taskerHome = $_SERVER['HOME'] . '/.tasker';
+
+            Storage::makeDirectory($taskerHome);
+
+            return true;
+        } );
+
+        $bar->advance();
+
+        $this->task('Creating Deployments Folder', function () {
+            sleep(1);
+
+            $taskerHome = $_SERVER['HOME'] . '/.tasker';
+
+            Storage::makeDirectory($taskerHome . '/deployments');
+
+            return true;
+        });
+
+        $this->task('Creating Database Folder', function () {
+            sleep(1);
+
+            $taskerHome = $_SERVER['HOME'] . '/.tasker';
+
+            Storage::makeDirectory($taskerHome . '/database');
+
+            return true;
+        });
+
+        $bar->advance();
+
+        $this->task('Create Database', function () {
+            sleep(1);
+
+            $taskerHome = $_SERVER['HOME'] . '/.tasker';
+
+            touch($taskerHome . '/database/database.sqlite');
+
+            return true;
+        });
+
+        $bar->advance();
+
+        $this->task('Migrate Database', function () {
+            sleep(1);
+
+            $this->call('migrate');
+
+            return true;
+        });
+
+        $bar->advance();
+
+
+        $bar->finish();
+
+        $this->info('Successfully Installed.');
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
